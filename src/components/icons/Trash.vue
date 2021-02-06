a1f9fce9adbfdc8c659f5b33864a5264cae0fd0f
Rasul <gad-ras1@yandex.ru>
2021-02-06 23:22:35 +0300

    Fix icon view in Chrome

--- src/components/icons/Trash.vue
+++ src/components/icons/Trash.vue
@@ -5,6 +5,7 @@
     viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg"
     v-bind="$attrs"
+    :class="$style.svg"
   >
     <path
       stroke-linecap="round"
@@ -20,3 +21,9 @@
     name: 'Trash',
   };
 </script>
+
+<style module lang="scss">
+  .svg {
+    width: 100%;
+  }
+</style>
